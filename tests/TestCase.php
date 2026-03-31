a880e14f378571ad7a4c1a1f3c07a21d34a84724
Bougies Stock Agent <aurelien.tisserand18@gmail.com>
2026-03-31 12:32:08 +0200

    T1.3: Fix tests LieuController - correction pluralisation, routes API, providers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,4 +8,24 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use RefreshDatabase;
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        // Load custom providers
+        $providers = require __DIR__.'/../bootstrap/providers.php';
+        foreach ($providers as $provider) {
+            $app->register($provider);
+        }
+
+        $app->make(\Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
